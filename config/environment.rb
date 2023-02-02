8043b635f6778cd68523fec520c9826e6681a5cc
Minchul An <min@minchulan.io>
2023-02-02 14:24:30 -0700

    commit tree c46e2d75799039b1c39e2f786a4d8eac73cd940a

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,13 @@
-# Set up Bundler here following the instructions in the README
+require 'bundler/setup'
+Bundler.require(:default, :development)
+
+source "https://rubygems.org"
+gem 'awesome_print', '~> 1.8'
+
+source "https://rubygems.org"
+gem "rspec"
+gem "rest-client"
+
+group :development do
+  gem "pry"
+end
